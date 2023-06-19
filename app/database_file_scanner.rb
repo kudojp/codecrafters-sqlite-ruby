dd0f0b6b1f48af523551e1f68dab1472d1747e82
kudojp <heyjudejudejude1968@gmail.com>
2023-06-20 01:21:02 +0900

    Refactor with DatabaseFileScanner#count_records

--- app/database_file_scanner.rb
+++ app/database_file_scanner.rb
@@ -11,16 +11,24 @@ class DatabaseFileScanner
     @file = File.open(database_file_path, "rb")
   end
 
-  def get_header_info
-    Database::HeaderInfo.new(page_size: page_size)
+  def header_info
+    @header_info = Database::HeaderInfo.new(page_size: page_size)
   end
 
-  def get_sqlite_schema
-    sqlite_schema = Database::SqliteSchema.new
+  def sqlite_schema
+    @sqlite_schema ||= get_sqlite_schema
+  end
 
-    sqlite_schema.cnt_tables = TableBTreeTraverser.new(@file, page_size, SQLITE_SCHEMA_PAGE_NUMBER, Database::SqliteSchema::TABLE_ATTRIBUTES).cnt_records
-    sqlite_schema.tables = TableBTreeTraverser.new(@file, page_size, SQLITE_SCHEMA_PAGE_NUMBER, Database::SqliteSchema::TABLE_ATTRIBUTES).get_records
-    sqlite_schema
+  def count_records(table_name)
+    table_info = self.sqlite_schema.tables.find{|tbl| tbl.fetch(:name) == table_name}
+    table_root_page_index = table_info.fetch(:rootpage)
+
+    TableBTreeTraverser.new(
+      @file,
+      self.page_size,
+      table_root_page_index,
+      nil
+    ).cnt_records
   end
 
   private
@@ -32,5 +40,13 @@ class DatabaseFileScanner
   def get_page_size
     @file.seek(PAGE_SIZE_OFFSET_IN_FILE_HEADER)
     @file.read(PAGE_SIZE_LENGTH_IN_FILE_HEADER).unpack("n")[0] # n: unsigned short (16-bit) in network byte order (= big-endian)
+  end
+
+  def get_sqlite_schema
+    sqlite_schema = Database::SqliteSchema.new
+
+    sqlite_schema.cnt_tables = TableBTreeTraverser.new(@file, page_size, SQLITE_SCHEMA_PAGE_NUMBER, Database::SqliteSchema::TABLE_ATTRIBUTES).cnt_records
+    sqlite_schema.tables = TableBTreeTraverser.new(@file, page_size, SQLITE_SCHEMA_PAGE_NUMBER, Database::SqliteSchema::TABLE_ATTRIBUTES).get_records
+    sqlite_schema
   end
 end
--- app/main.rb
+++ app/main.rb
@@ -8,26 +8,16 @@ scanner = DatabaseFileScanner.new(database_file_path)
 
 case command
 when ".dbinfo"
-  header_info = scanner.get_header_info
+  header_info = scanner.header_info
   puts "database page size: #{header_info.page_size}"
 
-  sqlite_schema = scanner.get_sqlite_schema
+  sqlite_schema = scanner.sqlite_schema
   puts "number of tables: #{sqlite_schema.cnt_tables}"
 when ".tables"
-  sqlite_schema = scanner.get_sqlite_schema
+  sqlite_schema = scanner.sqlite_schema
   puts sqlite_schema.tables.map{|tbl| tbl.fetch(:name)}.join " "
 else
   # Assuming that th command is "SELECT COUNT(*) FROM my_table;"
   table_name = command.split(" ")[3]
-  tables = scanner.get_sqlite_schema.tables
-  table = tables.find{|tbl| tbl.fetch(:name) == table_name}
-  root_page_index = table.fetch(:rootpage)
-
-  header_info = scanner.get_header_info
-  puts DatabaseFileScanner::TableBTreeTraverser.new(
-    File.open(database_file_path, "rb"),
-    header_info.page_size,
-    root_page_index,
-    nil
-  ).cnt_records
+  puts scanner.count_records(table_name)
 end
